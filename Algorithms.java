3686c53cc1d3eaf401795185b6aac1bb34accac0
BrianZ95 <brianz0905@gmail.com>
2024-12-05 03:57:47 +0000

    Initial Commit

--- Algorithms.java
+++ Algorithms.java
@@ -3,90 +3,69 @@ import java.io.File;
 import java.io.FileNotFoundException;
 
 public class Algorithms {
-    private static File f;
-    private static Scanner s;
-    public static void main(String[] args) throws FileNotFoundException {
-        f = new File("Numbers.txt");
-        int odds = odds();
-        int evens = evens();
-        int twodigit = twodigit();
-        int smallest = smallest();
-        int fivehundo = fivehundo();
-        int biggest= biggest();
-        int sum= sum();
-        System.out.println(odds);
-        s.close();
-    }
+    public static void main(String[] args) {
+        try {
+            File f = new File("Numbers.txt");
+            Scanner s = new Scanner(f);
 
-    public static int odds() throws FileNotFoundException{
-        s = new Scanner(f);
-        int odds = 0;
-        while (s.hasNext()) {
-            if (s.nextInt() % 2 != 0)
-                odds++;
-        }
-        return odds;
-    }    
+            // Variables for statistics
+            int odds = 0, evens = 0, twoDigit = 0, greaterThan500 = 0, sum = 0;
+            int smallest = Integer.MAX_VALUE, biggest = Integer.MIN_VALUE;
+            int[] frequency = new int[1001]; // Array for mode, assuming numbers in range 0 to 1000
+            int count = 0;
 
-    public static int evens() throws FileNotFoundException{
-        s = new Scanner(f);
-        int evens = 0;
-        while (s.hasNext()) {
-            if (s.nextInt() % 2 == 0)
-                evens++;
-        }
-        return evens;
-    }
+            // Process file
+            while (s.hasNextInt()) {
+                int num = s.nextInt();
+                count++;
+                sum += num;
 
-        public static int twodigit() throws FileNotFoundException{
-        s = new Scanner(f);
-        int twodigit = 0;
-        while (s.hasNext()) {
-            if (s.nextInt()>9 && s.nextInt()<100)
-                twodigit++;
-        }
-        return twodigit;
-    }
+                // Odd or even
+                if (num % 2 == 0) evens++;
+                else odds++;
 
-        public static int fivehundo() throws FileNotFoundException{
-        s = new Scanner(f);
-        int twodigit = 0;
-        while (s.hasNext()) {
-            if (s.nextInt()>500)
-                fivehundo++;
-        }
-        return fivehundo;
-    }
+                // Two-digit check
+                if (num >= 10 && num <= 99) twoDigit++;
 
-    public static int smallest() throws FileNotFoundException{
-        s = new Scanner(f);
-        int twodigit = 0;
-        while (s.hasNext()) {
-            if (s.nextInt()<s.previousInt())
-                smallest=s.nextInt();
-        }
-        return smallest;
-    }
+                // Greater than 500
+                if (num > 500) greaterThan500++;
 
-    public static int biggest() throws FileNotFoundException{
-        s = new Scanner(f);
-        int twodigit = 0;
-        while (s.hasNext()) {
-            if (s.nextInt()>s.previousInt())
-                biggest=s.nextInt();
-        }
-        return biggest;
-    }
+                // Smallest and biggest
+                if (num < smallest) smallest = num;
+                if (num > biggest) biggest = num;
 
-    public static int sum() throws FileNotFoundException{
-        s = new Scanner(f);
-        int twodigit = 0;
-        while (s.hasNext()) {
-            next=s.nextInt();
-            previous=s.previousInt();
-            
-            sum= next+previous;
+                // Increment frequency for mode
+                if (num >= 0 && num < frequency.length) {
+                    frequency[num]++;
+                }
+            }
+            s.close();
+
+            // Calculate mode
+            int mode = 0, maxFrequency = 0;
+            for (int i = 0; i < frequency.length; i++) {
+                if (frequency[i] > maxFrequency) {
+                    maxFrequency = frequency[i];
+                    mode = i;
+                }
+            }
+
+            // Calculate average
+            double average = (double) sum / count;
+
+            // Print results
+            System.out.println("Odd numbers: " + odds);
+            System.out.println("Even numbers: " + evens);
+            System.out.println("Two-digit numbers: " + twoDigit);
+            System.out.println("Numbers > 500: " + greaterThan500);
+            System.out.println("Smallest number: " + smallest);
+            System.out.println("Greatest number: " + biggest);
+            System.out.println("Sum of numbers: " + sum);
+            System.out.printf("Arithmetic mean: %.3f%n", average);
+            System.out.println("Mode of numbers: " + mode);
+
+        } catch (FileNotFoundException e) {
+            System.out.println("File not found: " + e.getMessage());
         }
-        return sum;
     }
 }
